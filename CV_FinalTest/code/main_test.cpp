508a983ac11a1364c6da055b2625940cdb8a17da
nic-ras <nic.ras89@gmail.com>
2025-09-11 19:35:05 +0200

    varie

--- CV_FinalTest/code/main_test.cpp
+++ CV_FinalTest/code/main_test.cpp
@@ -43,7 +43,7 @@ int main() {
 
     for (size_t i = 0; i < results.size(); i++) {
         // ridimensiono l'originale alla stessa dimensione della trasformata
-        float scale = 0.1f;
+        float scale = 0.2f;
         cv::Mat img = images[i].clone();
         cv::Mat displayImg;
         cv::resize(img, displayImg, cv::Size(), scale, scale);
